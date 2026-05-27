a766ee3a20928020de43ea4badaafdf6a05b2b27
Ronald Allan Rivera <jaeron.rivera@gmail.com>
2026-05-27 21:48:30 +0800

    chore: add shared @healthlog/eslint-config package

---
+++ tooling/eslint-config/base.js
@@ -0,0 +1,39 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettier from "eslint-config-prettier";
+import globals from "globals";
+
+export default tseslint.config(
+  {
+    ignores: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/.next/**",
+      "**/.expo/**",
+      "**/.turbo/**",
+      "**/coverage/**",
+      "**/*.config.js",
+      "**/*.config.mjs",
+      "**/*.config.ts",
+    ],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettier,
+  {
+    languageOptions: {
+      globals: { ...globals.node },
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        { prefer: "type-imports" },
+      ],
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+    },
+  },
+);
